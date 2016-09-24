eea46e18810b6df92db53fd20cf98969957ba4af
Andrew Rothwell <a_rothwell@outlook.com>
2016-09-24 04:38:09 -0400

    Beginning Implmentation of maze creation

--- EmotionMaze.py
+++ EmotionMaze.py
@@ -1,11 +1,68 @@
 #Must Install numpy package before runtime
 #Must Install easyTinker before runtime
 import numpy as np
+import random
+
+class Square:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.left = True
+        self.right = True
+        self.top = True
+        self.bottom = True
+        self.haswall = True
+        self.visited = False
+
+class Node:
+    def __init__(self,squareOne,squareTwo):
+        self.squareOne = squareOne
+        self.squareTwo = squareTwo
+
 tall = 10
 wide = 10
-maze = np.ones((tall, wide,4))
-print(maze)
+maze = list()
+visited = list()
+for x in range(0,tall):
+    for y in range(0,wide):
+        maze.append(Square(x,y))
+
+startX = random.randint(0, tall)
+startY = random.randint(0, wide)
+def mazeCreate():
+    if(findneighbor(startX,startY) == 0):
+        createnode(startX, startY, 0)
+    elif(findneighbor(startX,startY) == 1):
+        createnode(startX, startY, 1)
+    elif (findneighbor(startX, startY) == 2):
+        createnode(startX, startY, 2)
+    elif (findneighbor(startX, startY) == 3):
+        createnode(startX, startY, 3)
+    else:
+        if not allTried():
+            backup()
+            mazeCreate()
+        else:
+            #implement graphics here
+            temp = -1
 
 
 
+def findneighbor(startX,startY):
+    neighborWall = random.randint(0,4)
+    if(neighborWall == 0 and startX != 0):
+        return 0
+    else:
+        return False
 
+
+def visited(square):
+        return square.visited
+
+def createnode():
+    ver = 7
+
+def allTried():
+    ver = 8
+def backup():
+    temp = -1
