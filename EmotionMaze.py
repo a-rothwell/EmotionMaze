e528c6900d4b7547287e85e5ddbe1ad7a190751f
Andrew Rothwell <a_rothwell@outlook.com>
2016-09-24 10:58:17 -0400

    Implentation of node creation

--- EmotionMaze.py
+++ EmotionMaze.py
@@ -31,13 +31,13 @@ startX = random.randint(0, tall)
 startY = random.randint(0, wide)
 
 def mazeCreate():
-    if(findneighbor(startX,startY) == 0 and not visited(maze.index(startX * 10 + startY - 10))):
+    if findneighbor(startX,startY) == 0 and not maze[startX - 1][startY].visited:
         createnode(startX, startY, 0)
-    elif(findneighbor(startX,startY) == 1 and not visited(maze.index(startX * 10 + startY - 1))):
+    elif findneighbor(startX,startY) == 1 and not maze[startX][startY - 1].visited:
         createnode(startX, startY, 1)
-    elif (findneighbor(startX, startY) == 2 and not visited(maze.index(startX * 10 + startY + 10))):
+    elif findneighbor(startX, startY) == 2 and not maze[startX + 1][startY].visited:
         createnode(startX, startY, 2)
-    elif (findneighbor(startX, startY) == 3 and not visited(maze.index(startX * 10 + startY + 1))):
+    elif findneighbor(startX, startY) == 3 and not maze[startX][startY - 1].visited:
         createnode(startX, startY, 3)
     else:
         if not allTried():
@@ -62,51 +62,50 @@ def findneighbor(startX,startY):
         return -1
 
 
-def visited(square):
-        return square.visited
 
 def createnode(startX, startY, int):
     startingIndex = (startX * 10 + startY)
     if(int == 0):
-        nodes.append(Node(maze.index((startingIndex)),maze.index(startingIndex-10)))
-        maze.index(startingIndex - 10).visited = True
-        maze.index((startingIndex)).left = False
-        maze.index(startingIndex - 10).right = False
-        visited.append(maze.index(startingIndex - 10))
-        recenter(maze.index(startingIndex-10))
+        nodes.append(Node(maze[startX][startY],maze[startX - 1][startY]))
+        maze[startX - 1][startY].visited = True
+        maze[startX][startY].left = False
+        maze[startX - 1][startY].right = False
+        visited.append(maze[startX - 1][startY])
+        recenter(maze[startX - 1][startY])
 
     elif(int == 1):
-        nodes.append(Node(maze.index(startingIndex), maze.index(startingIndex - 1)))
-        maze.index(startingIndex - 1).visited = True
+        nodes.append(Node(maze[startX][startY], maze[startX][startY - 1]))
+        maze[startX][startY - 1].visited = True
         maze.index((startingIndex)).top = False
-        maze.index(startingIndex - 1).bottom = False
-        visited.append(maze.index(startingIndex - 1))
-        recenter(maze.index(startingIndex - 1))
+        maze[startX][startY - 1].bottom = False
+        visited.append(maze[startX][startY - 1])
+        recenter(maze[startX][startY - 1])
 
     elif (int == 2):
-        nodes.append(Node(maze.index(startingIndex), maze.index(startingIndex + 10)))
-        maze.index(startingIndex + 10).visited = True
-        maze.index((startingIndex)).right = False
-        maze.index(startingIndex + 10).left = False
-        visited.append(maze.index(startingIndex + 10))
-        recenter(maze.index(startingIndex + 10))
+        nodes.append(Node(maze[startX][startY], maze[startX + 1][startY]))
+        maze[startX + 1][startY].visited = True
+        maze[startX][startY].right = False
+        maze[startX + 1][startY].left = False
+        visited.append(maze[startX + 1][startY])
+        recenter(maze[startX + 1][startY])
 
     elif (int == 3):
-        nodes.append(Node(maze.index(startingIndex), maze.index(startingIndex + 1)))
-        maze.index(startingIndex + 1).visited = True
-        maze.index((startingIndex)).bottom = False
-        maze.index(startingIndex + 1).top = False
-        visited.append(maze.index(startingIndex + 1))
-        recenter(maze.index(startingIndex + 1))
+        nodes.append(Node(maze[startX][startY], maze[startX][startY + 1]))
+        maze[startX][startY + 1].visited = True
+        maze[startX][startY].bottom = False
+        maze[startX][startY + 1].top = False
+        visited.append(maze[startX][startY + 1])
+        recenter(maze[startX][startY + 1])
 
 def allTried():
-    if(len(maze) == len(visited)):
+    if len(maze) == len(visited):
         return True
     else:
         return False
 
 def backup():
     temp = -1
+
 def recenter(square):
     startX = square.x
     startY = square.y
