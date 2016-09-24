17a4cc1a30a7b3b4efecee8afc5bd8953c304ec0
Connor Borzello <connorborz@gmail.com>
2016-09-24 14:53:33 -0400

    Separated Graphics from Emotion Maze added a player that can move through maze.  Still needs to be restrained within boundaries however

--- Graphics.py
+++ Graphics.py
@@ -7,7 +7,7 @@ screenWidth = root.winfo_screenwidth()
 screenHeight = root.winfo_screenheight()
 # Resize tk window and sets its location
 root.geometry('%dx%d+%d+%d' % (screenWidth, screenHeight, -9, 0))
-root.resizable(FALSE,FALSE)
+root.resizable(FALSE, FALSE)
 
 # Frame is a widget container
 frame = Frame(root, background="gray", width=screenWidth, height=50)
@@ -16,6 +16,13 @@ frame.pack(side=TOP)
 # Canvas is used to draw things in tkinter
 canvas = Canvas(root, width=screenWidth, height=screenHeight)
 canvas.pack()
+numBoxes = 10
+boxWidth = (screenHeight - 100) / numBoxes
+topBorderLine = canvas.create_line(screenWidth/2-numBoxes/2*boxWidth, 50, screenWidth/2+numBoxes/2*boxWidth, 50)
+bottomBorderLine = canvas.create_line(screenWidth/2-numBoxes/2*boxWidth, boxWidth*numBoxes, screenWidth/2+numBoxes/2*boxWidth, boxWidth*numBoxes)
+leftBorderLine = canvas.create_line(screenWidth/2-numBoxes/2*boxWidth, 50, screenWidth/2-numBoxes/2*boxWidth, boxWidth * numBoxes)
+rightBorderLine = canvas.create_line(screenWidth/2+numBoxes/2*boxWidth, 50, screenWidth/2+numBoxes/2*boxWidth, boxWidth * numBoxes)
+
 
 class Player:
     """Represents a Player Object"""
@@ -33,21 +40,40 @@ class Player:
     def playerCanMoveDown(self):
         return True
     def movePlayerLeft(self, event) :
-        if self.playerCanMoveLeft :
-            self.xLocation -= self.speed
-        print("PLayer moving Left")
+        count = 0
+        while(count < self.speed) :
+            if self.playerCanMoveLeft :
+                self.xLocation -= 1
+                count+=1
+            else:
+                break
     def movePlayerRight(self, event):
-        if self.playerCanMoveRight :
-            self.xLocation += self.speed
+        count = 0
+        while count < self.speed :
+            if self.playerCanMoveRight :
+                self.xLocation += 1
+                count += 1
+            else :
+                break
     def movePlayerUp(self, event):
-        if self.playerCanMoveUp :
-            self.yLocation -= self.speed
+        count = 0
+        while count < self.speed :
+            if self.playerCanMoveUp :
+                self.yLocation -= 1
+                count += 1
+            else :
+                break
     def movePlayerDown(self, event):
-        if self.playerCanMoveDown:
-            self.yLocation += self.speed
+        count = 0
+        while count < self.speed :
+            if self.playerCanMoveDown:
+                self.yLocation += 1
+                count += 1
+            else :
+                break
 
 
-player = Player(20, 20, 10)
+player = Player(20, 20, boxWidth/2)
 playerPiece = canvas.create_rectangle(player.xLocation, player.yLocation, player.xLocation+player.size+1, player.yLocation+player.size+1, fill="red")
 # canvas.delete("all")
 root.grid()
