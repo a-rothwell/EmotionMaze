d42b8cd058aaa6dc094c78f3cf7504ce0c30ab07
Andrew Rothwell <a_rothwell@outlook.com>
2016-09-24 02:41:08 -0400

    Array Creation of Maze

--- EmotionMaze.py
+++ EmotionMaze.py
@@ -1,1 +1,11 @@
-print("potato")
+#Must Install numpy package before runtime
+#Must Install easyTinker before runtime
+import numpy as np
+tall = 10
+wide = 10
+maze = np.ones((tall, wide,4))
+print(maze)
+
+
+
+
